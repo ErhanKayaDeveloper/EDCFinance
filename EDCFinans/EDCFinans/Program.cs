03a8b625441270d61f167e0ebd3205b2ac2ce900
ErhanKayaDeveloper <robotikegitimicom@gmail.com>
2022-03-23 12:21:02 +0300

    Merhaba

--- EDCFinans/EDCFinans/Program.cs
+++ EDCFinans/EDCFinans/Program.cs
@@ -13,7 +13,7 @@ namespace EDCFinans
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();Console.WriteLine("Merhaba Bilgen");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
